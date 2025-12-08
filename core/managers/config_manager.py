697be5fb5d4d51551d5cdfc6fe2e78e3998175dc
Manuel Jesús Niza Cobo <mannizcob@alum.us.es>
2025-12-08 19:59:46 +0100

    🐛 fix: Se configura el CI

--- core/managers/config_manager.py
+++ core/managers/config_manager.py
@@ -7,9 +7,11 @@ class ConfigManager:
         self.app = app
 
     def load_config(self, config_name=None):
+        # Determina el entorno
         if config_name is None:
             config_name = os.getenv("FLASK_ENV", "development")
 
+        # Carga la configuración correspondiente
         if config_name == "testing":
             self.app.config.from_object(TestingConfig)
         elif config_name == "production":
@@ -17,15 +19,23 @@ class ConfigManager:
         else:
             self.app.config.from_object(DevelopmentConfig)
 
+        # Aseguramos que la app tenga la SECRET_KEY
+        if not self.app.secret_key:
+            self.app.secret_key = self.app.config.get("SECRET_KEY")
+
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_hex(32))
+    # Secret key
+    SECRET_KEY = os.getenv("SECRET_KEY") or secrets.token_hex(32)
+
+    # SQLAlchemy
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Config generales
     TIMEZONE = "Europe/Madrid"
     TEMPLATES_AUTO_RELOAD = True
     UPLOAD_FOLDER = "uploads"
 
-    # Detect DATABASE_URL (Render) or construir manualmente
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or (
         f"postgresql+psycopg2://{os.getenv('POSTGRES_USER', 'default_user')}:"
         f"{os.getenv('POSTGRES_PASSWORD', 'default_password')}@"
@@ -37,23 +47,27 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # Opcional: valores locales por defecto
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or (
-        "postgresql+psycopg2://local_user:local_pass@localhost:5432/local_db"
+        f"postgresql+psycopg2://{os.getenv('POSTGRES_USER', 'materialhub_user')}:"
+        f"{os.getenv('POSTGRES_PASSWORD', 'materialhub_password')}@"
+        f"{os.getenv('POSTGRES_HOSTNAME', 'localhost')}:"
+        f"{os.getenv('POSTGRES_PORT', '5432')}/"
+        f"{os.getenv('POSTGRES_DATABASE', 'materialhub')}"
     )
 
 
 class TestingConfig(Config):
     TESTING = True
+    WTF_CSRF_ENABLED = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or (
-        f"postgresql+psycopg2://{os.getenv('POSTGRES_USER', 'default_user')}:"
-        f"{os.getenv('POSTGRES_PASSWORD', 'default_password')}@"
+        f"postgresql+psycopg2://{os.getenv('POSTGRES_USER', 'materialhub_user')}:"
+        f"{os.getenv('POSTGRES_PASSWORD', 'materialhub_password')}@"
         f"{os.getenv('POSTGRES_HOSTNAME', 'localhost')}:"
         f"{os.getenv('POSTGRES_PORT', '5432')}/"
-        f"{os.getenv('POSTGRES_TEST_DATABASE', 'default_test_db')}"
+        f"{os.getenv('POSTGRES_TEST_DATABASE', 'materialhub_test')}"
     )
-    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
